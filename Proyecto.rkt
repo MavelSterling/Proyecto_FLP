ec01e3889ae6a22c0081d7a3142bfed0bed62b0b
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-06-29 19:21:08 -0500

    Se crea el archivo de racket

---
+++ Proyecto.rkt
@@ -0,0 +1,10 @@
+#lang eopl
+
+;; Proyecto final
+;;Grupo: 01.
+
+;;Integrantes
+;;#. Nombres completos            -   Código estudiantil
+;;1. Sebastián Tutistar Valencia           2110309
+;;2. Mavelyng Sterling Londoño             1430871
+;;3. Jefersson Danilo Arévalo              1926167   

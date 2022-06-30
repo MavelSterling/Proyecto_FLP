6d3f4d7ec60d8cb5edf6dab77fe4d85d30aad3b3
xlPlanetarizaGol <107218018+xlPlanetarizaGol@users.noreply.github.com>
2022-06-29 19:27:38 -0500

    Se actualizó la definición BNF

--- Proyecto.rkt
+++ Proyecto.rkt
@@ -9,4 +9,24 @@
 ;;2. Mavelyng Sterling Londoño             1430871
 ;;3. Jefersson Danilo Arévalo              1926167
 
-adsadasdas
+;Definición Backus-Naur form (BNF) para las expresiones del lenguaje:
+;
+;  <program>       ::= <expression>
+;                      <a-program (exp)>
+;  <expression>    ::= <number>
+;                      <lit-exp (datum)>
+;                  ::= <identifier>
+;                      <var-exp (id)>
+;                  ::= <primitive> ({<expression>}*(,))
+;                      <primapp-exp (prim rands)>
+;                  ::= if <expresion> then <expresion> else <expression>
+;                      <if-exp (exp1 exp2 exp23)>
+;                  ::= let {identifier = <expression>}* in <expression>
+;                      <let-exp (ids rands body)>
+;                  ::= proc({<identificador>}*(,)) <expression>
+;                      <proc-exp (ids body)>
+;                  ::= (<expression> {<expression>}*)
+;                      <app-exp proc rands>
+;  <primitive>     ::= + | - | * | add1 | sub1 
+
+

24f97e224dcb18b1b0405f181acdd8f361cd041f
xlPlanetarizaGol <107218018+xlPlanetarizaGol@users.noreply.github.com>
2022-06-29 19:24:14 -0500

    Update Proyecto.rkt

--- Proyecto.rkt
+++ Proyecto.rkt
@@ -7,4 +7,6 @@
 ;;#. Nombres completos            -   Código estudiantil
 ;;1. Sebastián Tutistar Valencia           2110309
 ;;2. Mavelyng Sterling Londoño             1430871
-;;3. Jefersson Danilo Arévalo              1926167   
+;;3. Jefersson Danilo Arévalo              1926167
+
+adsadasdas
